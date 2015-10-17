655304dc8b63d142204fb10f8ffbdbec8e49df0a
Tom <redsoxfantom@gmail.com>
2015-10-17 19:41:33 -0400

    Added code to read and deserialize an object

--- EmulatorManager/EmulatorManager/Utilities/FileManager.cs
+++ EmulatorManager/EmulatorManager/Utilities/FileManager.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,10 @@ namespace EmulatorManager.Utilities
     {
         public static T LoadObject<T>(string Path)
         {
+            String serializedObject = File.ReadAllText(Path);
+            T deserializedObject = JsonConvert.DeserializeObject<T>(serializedObject);
 
+            return deserializedObject;
         }
 
         public static void SaveObject(object Obj, string Path)
