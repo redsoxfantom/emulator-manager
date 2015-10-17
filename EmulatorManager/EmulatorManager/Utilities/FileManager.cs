770c06cac939361b5f58e94190d2d05c8250cd0b
Tom <redsoxfantom@gmail.com>
2015-10-17 19:39:18 -0400

    Added reference to newtonsoft and began work on file manager

---
+++ EmulatorManager/EmulatorManager/Utilities/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Utilities
+{
+    public class FileManager
+    {
+        public static T LoadObject<T>(string Path)
+        {
+
+        }
+
+        public static void SaveObject(object Obj, string Path)
+        {
+            String serializedObject = JsonConvert.SerializeObject(Obj);
+        }
+    }
+}
