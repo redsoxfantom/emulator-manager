0d6b4fc929ca215d65334c88b9d3db2466ee00ca
Tom <redsoxfantom@gmail.com>
2015-10-17 19:18:14 -0400

    Defined Emulator and RomPath contracts

--- EmulatorManager/EmulatorManager/GlobalManager/DataContracts/Emulator.cs
+++ EmulatorManager/EmulatorManager/GlobalManager/DataContracts/Emulator.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmulatorManager.GlobalManager.DataContracts
 {
+    [DataContract(Name ="Emulator")]
     public class Emulator
     {
+        [DataMember(Name ="Path",Order =0)]
+        public string Path;
+
+        [DataMember(Name ="Executable",Order =1)]
+        public string Executable;
+
+        [DataMember(Name ="Arguments",Order =2)]
+        public string Arguments;
     }
 }
--- EmulatorManager/EmulatorManager/GlobalManager/DataContracts/RomPath.cs
+++ EmulatorManager/EmulatorManager/GlobalManager/DataContracts/RomPath.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmulatorManager.GlobalManager.DataContracts
 {
+    [DataContract(Name ="RomPath")]
     public class RomPath
     {
+        [DataMember(Name ="Path",Order =0)]
+        public string FolderPath;
+
+        [DataMember(Name ="AssociatedEmulator",Order =1)]
+        public string AssociatedEmulator;
     }
 }
