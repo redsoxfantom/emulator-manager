5b80d5f21798c2f8258d130f58fbdd2a59148924
Tom <redsoxfantom@gmail.com>
2015-10-17 19:07:20 -0400

    Defined emulator manager config

--- EmulatorManager/EmulatorManager/GlobalManager/DataContracts/EmulatorManagerConfig.cs
+++ EmulatorManager/EmulatorManager/GlobalManager/DataContracts/EmulatorManagerConfig.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmulatorManager.GlobalManager.DataContracts
 {
+    [DataContract(Name ="Config")]
     public class EmulatorManagerConfig
     {
+        [DataMember(Name = "Emulators", Order = 0)]
+        public List<Emulator> Emulators;
 
+        [DataMember(Name ="Paths",Order =1)]
+        public List<RomPath> Paths;
     }
 }
