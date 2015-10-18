dbff5b2b147f65b65541b3f603ed23b510eaae59
Tom <redsoxfantom@gmail.com>
2015-10-18 19:56:34 -0400

    Added code to create a processor and logger

--- EmulatorManager/EmulatorManager/MainExecutable.cs
+++ EmulatorManager/EmulatorManager/MainExecutable.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace EmulatorManager
 {
     class MainExecutable
     {
         static void Main(string[] args)
         {
+            Processor proc = new Processor();
+
+            proc.Init(args);
+
+            // Execute and check the return code
+            if(proc.Execute())
+            {
+                Environment.Exit(0); // Success
+            }
+            else
+            {
+                Environment.Exit(1); // Failed
+            }
         }
     }
 }
--- EmulatorManager/EmulatorManager/Processor.cs
+++ EmulatorManager/EmulatorManager/Processor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,19 @@ namespace EmulatorManager
 {
     public class Processor
     {
+        private ILog mLogger;
+
         public void Init(string[] args)
         {
-
+            mLogger = LogManager.GetLogger(GetType().Name);
         }
 
         public bool Execute()
         {
+            mLogger.Info("Executing...");
+
+            Console.ReadLine();
+
             return true;
         }
     }
