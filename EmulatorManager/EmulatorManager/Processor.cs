8510f998632c22f0ff215abed93c579e6997f62f
Tom <redsoxfantom@gmail.com>
2015-10-18 19:47:42 -0400

    Created Processor class to handle actual execution instead of main method

---
+++ EmulatorManager/EmulatorManager/Processor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager
+{
+    public class Processor
+    {
+        public void Init(string[] args)
+        {
+
+        }
+
+        public bool Execute()
+        {
+            return true;
+        }
+    }
+}
