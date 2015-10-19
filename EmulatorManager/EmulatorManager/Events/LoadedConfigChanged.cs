ec8511d724b1e74e740cd29ccaa18d89055cfb13
Tom <redsoxfantom@gmail.com>
2015-10-18 20:35:58 -0400

    Created delegate and event args for when the loaded config changes

---
+++ EmulatorManager/EmulatorManager/Events/LoadedConfigChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Events
+{
+    public delegate void LoadedConfigChanged(LoadedConfigChangedArgs args);
+
+    public class LoadedConfigChangedArgs : EventArgs
+    {
+        string ConfigFileName;
+    }
+}
