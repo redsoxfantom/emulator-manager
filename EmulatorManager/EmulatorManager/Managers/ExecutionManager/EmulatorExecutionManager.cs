50b46ab8741d38977d225927888ab06193e87df2
Tom <redsoxfantom@gmail.com>
2015-10-20 17:51:47 -0400

    Refactored code that executes emulators to make it safer

--- EmulatorManager/EmulatorManager/Managers/ExecutionManager/EmulatorExecutionManager.cs
+++ EmulatorManager/EmulatorManager/Managers/ExecutionManager/EmulatorExecutionManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,51 @@ namespace EmulatorManager.Managers.ExecutionManager
 {
     public class EmulatorExecutionManager
     {
+        private ILog mLogger;
+
         public EmulatorExecutionManager()
         {
+            mLogger = LogManager.GetLogger(GetType().Name);
 
         }
 
-        public void ExecuteProcess(String Path, String Args)
+        public void ExecuteProcess(String Path, String Args, String Replacements)
         {
-            System.Diagnostics.Process.Start(Path, Args);
+            String finalArgs = "";
+            if(!String.IsNullOrEmpty(Args))
+            {
+                finalArgs = String.Format(Args, Replacements);
+            }
+
+            mLogger.Info(String.Format("Executing emulator {0} with arguments {1}",Path,finalArgs));
+            using (Process emulator = new Process())
+            {
+                try
+                {
+                    emulator.StartInfo.FileName = Path;
+                    emulator.StartInfo.Arguments = finalArgs;
+                    emulator.StartInfo.UseShellExecute = false;
+                    emulator.StartInfo.RedirectStandardOutput = true;
+                    emulator.StartInfo.RedirectStandardError = true;
+                    emulator.Start();
+
+                    emulator.WaitForExit();
+
+                    String stdOut = emulator.StandardOutput.ReadToEnd();
+                    String stdErr = emulator.StandardError.ReadToEnd();
+                    mLogger.Info(String.Format("Emulator exited. StdOut:\n{0}", stdOut));
+                    if(!String.IsNullOrEmpty(stdErr))
+                    {
+                        mLogger.Error(String.Format("Emulator exited with errors. StdErr:\n{0}",stdErr));
+                    }
+                }
+                catch(Exception ex)
+                {
+                    mLogger.Error("Failed to execute emulator process", ex);
+                }
+            }
+            
+
         }
     }
 }
