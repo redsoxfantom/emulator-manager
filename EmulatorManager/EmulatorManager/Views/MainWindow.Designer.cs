b62017d7e4e04870db1f01b857620e1d0446e628
Tom <redsoxfantom@gmail.com>
2015-10-20 18:20:33 -0400

    Changed font on treeview

--- EmulatorManager/EmulatorManager/Views/MainWindow.Designer.cs
+++ EmulatorManager/EmulatorManager/Views/MainWindow.Designer.cs
@@ -101,6 +101,7 @@
             // 
             // treeEmulatorView
             // 
+            this.treeEmulatorView.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.treeEmulatorView.Location = new System.Drawing.Point(13, 28);
             this.treeEmulatorView.Name = "treeEmulatorView";
             this.treeEmulatorView.Size = new System.Drawing.Size(825, 600);
