f562d14b02423ba08c2a7d396802a3ef5f6f4aeb
Tom <redsoxfantom@gmail.com>
2015-10-20 18:00:35 -0400

    Implemented path resolving

--- EmulatorManager/EmulatorManager/Components/PathComponent/PathResolverComponent.cs
+++ EmulatorManager/EmulatorManager/Components/PathComponent/PathResolverComponent.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace EmulatorManager.Components.PathComponent
 {
     public class PathResolverComponent
     {
+        private ILog mLogger;
+
+        public PathResolverComponent()
+        {
+            mLogger = LogManager.GetLogger(GetType().Name);
+        }
+
+        public List<String> ResolvePaths(String Folder)
+        {
+            mLogger.Info(String.Format("Searching folder {0} for files",Folder));
+            // Return a list of all files in a folder
+            List<String> pathList = new List<String>(Directory.GetFiles(Folder));
+
+            mLogger.Debug(String.Format("Search returned the following files:\n {0}",String.Join("\n",pathList.ToArray())));
+
+            return pathList;
+        }
     }
 }
