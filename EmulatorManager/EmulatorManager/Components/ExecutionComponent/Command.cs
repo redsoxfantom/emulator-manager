9af42bb5dc52d1c90a3407dae9e8ac1b1cb16d09
Tom <redsoxfantom@gmail.com>
2015-10-20 19:20:35 -0400

    Created command class to encapsulate a command to execute

---
+++ EmulatorManager/EmulatorManager/Components/ExecutionComponent/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Components.ExecutionComponent
+{
+    public class Command
+    {
+        public String ExecutionPath { get; private set; }
+
+        public String ExecutionArguments { get; private set; }
+
+        public Command(String executionPath, String executionArgs = "", String argReplacements = null)
+        {
+            if (!String.IsNullOrEmpty(executionArgs))
+            {
+
+            }
+        }
+    }
+}
