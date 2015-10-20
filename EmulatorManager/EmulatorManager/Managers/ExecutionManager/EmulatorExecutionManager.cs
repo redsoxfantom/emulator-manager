edbfa26da40c625c70dc5feb2437e9c0f77a120e
Tom <redsoxfantom@gmail.com>
2015-10-19 22:18:49 -0400

    Added manager to handle starting emulators given a name and a path

---
+++ EmulatorManager/EmulatorManager/Managers/ExecutionManager/EmulatorExecutionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Managers.ExecutionManager
+{
+    public class EmulatorExecutionManager
+    {
+        public EmulatorExecutionManager()
+        {
+
+        }
+
+        public void ExecuteProcess(String Path, String Args)
+        {
+            System.Diagnostics.Process.Start(Path, Args);
+        }
+    }
+}
