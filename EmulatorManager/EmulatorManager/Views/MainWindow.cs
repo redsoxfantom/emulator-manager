0552b6d0a38d9898a6ab36704586da3413c98f0b
Tom <redsoxfantom@gmail.com>
2015-10-22 19:31:31 -0400

    Added dummy context menu entry for right clicking emulator

--- EmulatorManager/EmulatorManager/Views/MainWindow.cs
+++ EmulatorManager/EmulatorManager/Views/MainWindow.cs
@@ -251,8 +251,9 @@ namespace EmulatorManager.Views
             Emulator emu = selectedNode.Emulator;
             mLogger.Info(String.Format("Handling emulator right click on emulator {0}", emu.ToString()));
             ContextMenuStrip ctxMenu = new ContextMenuStrip();
+            ToolStripMenuItem modifyEmulator = new ToolStripMenuItem(String.Format("Modify Emulator {0}",emu.Name));
 
-
+            ctxMenu.Items.Add(modifyEmulator);
             treeEmulatorView.ContextMenuStrip = ctxMenu;
         }
 
