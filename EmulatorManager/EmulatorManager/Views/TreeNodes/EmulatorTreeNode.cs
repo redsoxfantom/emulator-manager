7e1538881d557a3bffc4d58bfdcc1e44117915df
Tom <redsoxfantom@gmail.com>
2015-10-22 19:18:15 -0400

    Emulator and path tree nodes inherit from TreeNode

--- EmulatorManager/EmulatorManager/Views/TreeNodes/EmulatorTreeNode.cs
+++ EmulatorManager/EmulatorManager/Views/TreeNodes/EmulatorTreeNode.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EmulatorManager.Views.TreeNodes
 {
-    public class EmulatorTreeNode
+    public class EmulatorTreeNode : TreeNode
     {
     }
 }
