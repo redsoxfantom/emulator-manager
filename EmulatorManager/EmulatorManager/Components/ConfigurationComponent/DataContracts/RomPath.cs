1beed925a9a56e68bc795a072f5b7ab6dfd45930
Tom <redsoxfantom@gmail.com>
2015-10-21 21:29:19 -0400

    Added json properties to RomPath

--- EmulatorManager/EmulatorManager/Components/ConfigurationComponent/DataContracts/RomPath.cs
+++ EmulatorManager/EmulatorManager/Components/ConfigurationComponent/DataContracts/RomPath.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -9,12 +10,16 @@ namespace EmulatorManager.Components.ConfigurationManager.DataContracts
 {
     public class RomPath
     {
+        [JsonProperty]
         public int Id;
 
+        [JsonProperty]
         public string FolderPath;
-        
+
+        [JsonProperty]
         public string AssociatedEmulator;
 
+        [JsonProperty]
         public string RomExtension;
 
         public override string ToString()
