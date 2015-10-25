fb20fd1232a99a96154dd94c614a8beaf445fc23
Tom <redsoxfantom@gmail.com>
2015-10-25 14:12:48 -0400

    Added properties to GameData

---
+++ EmulatorManager/EmulatorManager/Components/GameDataComponent/GameData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Components.GameDataComponent
+{
+    public class GameData
+    {
+        public string GameName { get; private set; }
+
+        public string GamePublisher { get; private set; }
+
+        public string GameSystem { get; private set; }
+
+        public Image GameImage { get; private set; }
+    }
+}
