e551ecfe5c74a6c5dfb27db7fdfe51c450ec0f50
Tom <redsoxfantom@gmail.com>
2015-10-25 14:27:04 -0400

    Created constructor for GameData

--- EmulatorManager/EmulatorManager/Components/GameDataComponent/GameData.cs
+++ EmulatorManager/EmulatorManager/Components/GameDataComponent/GameData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmulatorManager.Properties;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -16,5 +17,22 @@ namespace EmulatorManager.Components.GameDataComponent
         public string GameSystem { get; private set; }
 
         public Image GameImage { get; private set; }
+
+        public GameData(
+            string gameName = "<No Game Name>", 
+            string gamePublisher = "<No Game Publisher>", 
+            string gameSystem = "<No Game System>", 
+            Image gameImage = null)
+        {
+            GameName = gameName;
+            GamePublisher = gamePublisher;
+            GameSystem = gameSystem;
+
+            if(gameImage == null)
+            {
+                gameImage = Resources.No_Image_Found;
+            }
+            GameImage = gameImage;
+        }
     }
 }
