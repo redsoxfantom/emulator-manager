be731879d16b2ad6b1babbe4c6ac9b759d0fb04a
Tom <redsoxfantom@gmail.com>
2015-10-25 13:37:46 -0400

    Added interface for RomReaders

---
+++ EmulatorManager/EmulatorManager/Components/GameDataComponent/RomReaders/IRomReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Components.GameDataComponent.RomReaders
+{
+    public interface IRomReader
+    {
+    }
+}
