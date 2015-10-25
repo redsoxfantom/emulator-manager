f88446f373cb5dda494e71478191c7cf23bb2a49
Tom <redsoxfantom@gmail.com>
2015-10-25 13:57:37 -0400

    Created Gamecube rom reader

---
+++ EmulatorManager/EmulatorManager/Components/GameDataComponent/RomReaders/GamecubeRomReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Components.GameDataComponent.RomReaders
+{
+    public class GamecubeRomReader : IRomReader
+    {
+        public string RomType
+        {
+            get
+            {
+                return "GameCube";
+            }
+        }
+
+        public string GetRomId(FileStream rom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
