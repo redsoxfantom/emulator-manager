54bd001b664942330453470ab508d834af784628
Tom <redsoxfantom@gmail.com>
2015-10-25 00:02:28 -0400

    Created console application to read a rom file in chunks

---
+++ EmulatorManager/RomFileReader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processor proc = new Processor();
+        }
+    }
+}
