446909578cda7b77e2237b2aadf30b6bde339f2e
Tom <redsoxfantom@gmail.com>
2015-10-25 20:34:40 -0400

    RomDataAccessor can make an http request to the rest client and return the result as a string

--- EmulatorManager/EmulatorManager/Components/GameDataComponent/RomDataAccessor.cs
+++ EmulatorManager/EmulatorManager/Components/GameDataComponent/RomDataAccessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,27 @@ namespace EmulatorManager.Components.GameDataComponent
     {
         string mUrl;
 
+        HttpClient mCLient;
+
         public RomDataAccessor(string dataUrl)
         {
             mUrl = dataUrl;
+            mCLient = new HttpClient();
+        }
+
+        public async Task<GameData> LoadGameData(string romType, string romId)
+        {
+            string finalUrl = String.Format("{0}/api/{1}/{2}",mUrl,romType,romId);
+
+            using (HttpResponseMessage resp = await mCLient.GetAsync(finalUrl))
+            {
+                if (resp.IsSuccessStatusCode)
+                {
+                    string responseDataString = await resp.Content.ReadAsStringAsync();
+                }
+            }
+
+            return null;
         }
     }
 }
