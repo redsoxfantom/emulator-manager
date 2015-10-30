1959d6503abb406a1397ff8022f188454d21de92
Tom <redsoxfantom@gmail.com>
2015-10-29 21:04:39 -0400

    Added tags to GameData to define it's Id and whether it exists on the server or not

--- EmulatorManager/EmulatorManager/Components/GameDataComponent/GameData.cs
+++ EmulatorManager/EmulatorManager/Components/GameDataComponent/GameData.cs
@@ -18,15 +18,23 @@ namespace EmulatorManager.Components.GameDataComponent
 
         public Image GameImage { get; private set; }
 
+        public int Id { get; private set; }
+
+        public Boolean ExistsOnServer { get; private set; }
+
         public GameData(
             string gameName = "<No Game Name>", 
             string gamePublisher = "<No Game Publisher>", 
             string gameSystem = "<No Game System>", 
-            Image gameImage = null)
+            Image gameImage = null,
+            int id = -1,
+            Boolean existsOnServer = false)
         {
             GameName = gameName;
             GamePublisher = gamePublisher;
             GameSystem = gameSystem;
+            Id = id;
+            ExistsOnServer = existsOnServer;
 
             if(gameImage == null)
             {
--- EmulatorManager/EmulatorManager/Utilities/RestUtilities/RestServerManager.cs
+++ EmulatorManager/EmulatorManager/Utilities/RestUtilities/RestServerManager.cs
@@ -33,6 +33,6 @@ namespace EmulatorManager.Utilities.RestUtilities
                     }
                 }
             }
-        } 
+        }
     }
 }
