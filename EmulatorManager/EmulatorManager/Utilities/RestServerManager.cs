579ce24c056a652fb75cfb623c792ef706764656
Tom <redsoxfantom@gmail.com>
2015-10-29 20:45:23 -0400

    Created Get method on server manager

--- EmulatorManager/EmulatorManager/Utilities/RestServerManager.cs
+++ EmulatorManager/EmulatorManager/Utilities/RestServerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,12 @@ namespace EmulatorManager.Utilities
 {
     public class RestServerManager
     {
+        public async static Task<dynamic> Get(string url)
+        {
+            using (var client = new HttpClient())
+            {
+
+            }
+        } 
     }
 }
