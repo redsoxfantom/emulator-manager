507d60cd20e7b46bc615a0e4dc237c840cbfc427
Tom <redsoxfantom@gmail.com>
2015-10-29 22:00:10 -0400

    Initial work on Rom Data Update Form

---
+++ EmulatorManager/EmulatorManager/Views/UpdateRomDataServerWindow.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace EmulatorManager.Views
+{
+    partial class UpdateRomDataServerWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblImage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 198);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblImage
+            // 
+            this.lblImage.AutoSize = true;
+            this.lblImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImage.Location = new System.Drawing.Point(13, 13);
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(46, 17);
+            this.lblImage.TabIndex = 1;
+            this.lblImage.Text = "label1";
+            // 
+            // UpdateRomDataServerWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 301);
+            this.Controls.Add(this.lblImage);
+            this.Controls.Add(this.panel1);
+            this.Name = "UpdateRomDataServerWindow";
+            this.Text = "Add / Update Rom Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblImage;
+    }
+}
