a726298d9e294100f8d0de3b0dc6bfebb26e904c
Tom <redsoxfantom@gmail.com>
2015-11-16 19:37:25 -0500

    Created empty reader to handle PS2 roms

---
+++ EmulatorManager/EmulatorManager.Components/Components/GameDataComponent/RomReaders/PS2RomReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Components.GameDataComponent.RomReaders
+{
+    public class PS2RomReader : IRomReader
+    {
+        public bool TryReadRom(FileStream rom, out string RomId, out string RomType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
