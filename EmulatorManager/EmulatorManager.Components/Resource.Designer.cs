c6e859e4dc624daf07a8cb7fbd5c0a85f885f283
Tom <redsoxfantom@gmail.com>
2015-11-16 19:36:35 -0500

    Changed default namespace for components

--- EmulatorManager/EmulatorManager.Components/Resource.Designer.cs
+++ EmulatorManager/EmulatorManager.Components/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EmulatorManager.Components {
+namespace EmulatorManager {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace EmulatorManager.Components {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EmulatorManager.Components.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EmulatorManager.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
