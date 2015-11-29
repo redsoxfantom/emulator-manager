288343e55300e9a4065dbf4cace9a695fc7c821f
Tom <redsoxfantom@gmail.com>
2015-11-28 22:58:30 -0500

    Created component to handle access to the Joystick (if connected)

---
+++ EmulatorManager/EmulatorManager.Components/Components/InputComponent/JoystickComponent.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Components.InputComponent
+{
+    public class JoystickComponent
+    {
+        private ILog mLogger;
+
+        private JoystickComponent mInstance;
+
+        private JoystickComponent()
+        {
+            mLogger = LogManager.GetLogger(GetType().Name);
+        }
+
+        public JoystickComponent Instance
+        {
+            get
+            {
+                if(mInstance == null)
+                {
+                    mInstance = new JoystickComponent();
+                }
+                return mInstance;
+            }
+        }
+    }
+}
