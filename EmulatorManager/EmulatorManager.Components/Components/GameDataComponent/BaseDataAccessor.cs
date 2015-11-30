6fa277749075847a2a7dc05fbef3e3be1dd5418a
Tom <redsoxfantom@gmail.com>
2015-11-30 17:30:32 -0500

    Created base class for data accessors

---
+++ EmulatorManager/EmulatorManager.Components/Components/GameDataComponent/BaseDataAccessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulatorManager.Components.GameDataComponent
+{
+    class BaseDataAccessor : IRomDataAccessor
+    {
+        public BaseDataAccessor(string dataLocation)
+        {
+
+        }
+
+        public void ClearCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GameData> RetrieveGameData(string romType, string romId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateGamePlayedTime(string romId, GameData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrAddGameData(string romId, GameData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
