ee5804988609e6349a011769b044394d3fc9a8a0
Tom <redsoxfantom@gmail.com>
2015-12-02 18:48:16 -0500

    Get an instance of the connected joystick (if it exists)

--- EmulatorManager/EmulatorManager.Components/Components/InputComponent/JoystickComponent.cs
+++ EmulatorManager/EmulatorManager.Components/Components/InputComponent/JoystickComponent.cs
@@ -1,4 +1,5 @@
 ﻿using log4net;
+using SharpDX.DirectInput;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,25 @@ namespace EmulatorManager.Components.InputComponent
 
         private JoystickComponent mInstance;
 
+        private Joystick joystickInstance;
+
+        private DirectInput directInput;
+
         private JoystickComponent()
         {
             mLogger = LogManager.GetLogger(GetType().Name);
+
+            directInput = new DirectInput();
+
+            var joystickInstance = GetJoystickInstance();
+            if(joystickInstance == null)
+            {
+                mLogger.WarnFormat("No gamepad connected");
+            }
+            else
+            {
+                mLogger.InfoFormat("Gamepad detected. Name: {0}",joystickInstance.ProductName);
+            }
         }
 
         public JoystickComponent Instance
@@ -28,6 +45,16 @@ namespace EmulatorManager.Components.InputComponent
                 }
                 return mInstance;
             }
+        }
+
+        private DeviceInstance GetJoystickInstance()
+        {
+            var devices = directInput.GetDevices(DeviceClass.GameControl, DeviceEnumerationFlags.AttachedOnly);
+            if(devices.Count > 0)
+            {
+                return devices[0];
+            }
+            return null;
         }
     }
 }
