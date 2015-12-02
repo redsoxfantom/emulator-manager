3d78779ce53953da9e5b44541225170ba7602b18
Tom <redsoxfantom@gmail.com>
2015-12-01 20:23:41 -0500

    Implemented CanConvert

--- EmulatorManager/EmulatorManager.Components/Components/GameDataComponent/ImageConverter.cs
+++ EmulatorManager/EmulatorManager.Components/Components/GameDataComponent/ImageConverter.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace EmulatorManager.Components.GameDataComponent
     {
         public override bool CanConvert(Type objectType)
         {
-            throw new NotImplementedException();
+            return objectType.IsAssignableFrom(Image);
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
