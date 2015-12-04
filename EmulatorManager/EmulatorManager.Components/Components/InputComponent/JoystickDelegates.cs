683ed6a15c738778b70617f3d83bf9d918233d7b
Tom <redsoxfantom@gmail.com>
2015-12-03 21:06:07 -0500

    Created delegate for handling a joystick event

--- EmulatorManager/EmulatorManager.Components/Components/InputComponent/JoystickDelegates.cs
+++ EmulatorManager/EmulatorManager.Components/Components/InputComponent/JoystickDelegates.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpDX.DirectInput;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,4 +14,6 @@ namespace EmulatorManager.Components.InputComponent
     }
 
     public delegate void JoystickStatusChanged(JoystickStatus newStatus);
+
+    public delegate void JoystickInput(JoystickUpdate updateEvent);
 }
